4e2403bc57409ab22e0a0bcea47c10a10e76c88d
jon <janssen.nunag@gmail.com>
2023-02-28 10:10:05 -0500

    week 7 lec

---
+++ week7/server/config/db.js
@@ -0,0 +1,5 @@
+module.exports =
+{
+    //"URI":"mongodb://127.0.0.1/book_store"
+    "URI":"mongodb+srv://Janssen:g62YLo3eEwmyPPmU@comp229.efwa1iv.mongodb.net/book_store?retryWrites=true&w=majority"
+}
---
+++ week7/server/routes/book.js
@@ -0,0 +1,39 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+let passport = require('passport');
+
+//connect to our book model
+let Book = require('../model/books');
+
+let bookController = require('../controller/book');
+
+//helper function for guard puposes
+function requireAuth(req,res,next) {
+    //check if user is logged in
+    if(!req.isAuthenticated()) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+//GET ROUTE for the book list page - READ OPERATION
+router.get('/', bookController.displayBookList);
+
+//GET route for displaying the Add Page - CREATE operation
+router.get('/add', requireAuth, bookController.displayAddPage);
+
+//POST route for processing the Add Page - CREATE operation
+router.post('/add', requireAuth, bookController.processAddPage);
+
+//GET route for displaying the Edit Page - UPDATE operation
+router.get('/edit/:id', requireAuth, bookController.displayEditPage);
+
+//POST route for processing the Edit Page - UPDATE operation
+router.post('/edit/:id', requireAuth, bookController.processEditPage);
+
+//GET to perform deletion - DELETE operation
+router.get('/delete/:id', requireAuth, bookController.processDelete);
+
+module.exports = router;
