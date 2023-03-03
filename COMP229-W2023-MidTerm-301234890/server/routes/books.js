003f93bb0a40aae51cbfdf923a9849ee42286858
jon <janssen.nunag@gmail.com>
2023-03-03 14:43:17 -0500

    added code for delete book functionality

--- COMP229-W2023-MidTerm-301234890/server/routes/books.js
+++ COMP229-W2023-MidTerm-301234890/server/routes/books.js
@@ -104,6 +104,15 @@ router.get('/delete/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    let id = req.params.id;
+    book.remove({_id:id}, (err) => {
+        if(err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            res.redirect('/books');
+        }
+    });
 });
 
 
