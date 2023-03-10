505316bf9492f215b936b653bfd72c210d570ce6
jon <janssen.nunag@gmail.com>
2023-03-10 18:04:59 -0500

    week 8 lab

---
+++ week8/Client/data.js
@@ -0,0 +1,76 @@
+module.exports = function() {
+    return {
+        books:
+        [
+            {
+                "id": 1,
+                name: 'Great',
+                author: 'Bisi Great',
+                published: '1955',
+                description: 'In this great world of Goodness',
+                price: 19.95
+              },
+              {
+                "id": 2,
+                name: 'Chris',
+                author: 'Chris Blues',
+                published: '1960',
+                description: 'Inglo Chris',
+                price: 22.76
+              },
+              {
+                "id": 3,
+                name: 'Peace',
+                author: 'Peace Praise',
+                published: '1965',
+                description: 'Inglo Praise Peace',
+                price: 20.95
+              },
+              {
+                "id": 4,
+                name: 'Prayer',
+                author: 'DG',
+                published: '1976',
+                description: 'Is powerful',
+                price: 115,
+                __v: 0
+              },
+              {
+                "id": 5,
+                name: "Philosopher's Stone",
+                author: 'JK Rowling',
+                published: '1997',
+                description: 'first',
+                price: 10,
+                __v: 0
+              },
+              {
+                "id": 6,
+                name: 'Western Life',
+                author: 'westdee',
+                published: '1977',
+                description: 'ajsdoajsio',
+                price: 6,
+                __v: 0
+              },
+              {
+                "id": 7,
+                name: 'Hello',
+                author: 'Adele',
+                published: '2002',
+                description: "It's me",
+                price: 88,
+                __v: 0
+              },
+              {
+                "id": 8,
+                name: 'Book',
+                author: 'Author',
+                published: 'Year',
+                description: 'Description',
+                price: 12,
+                __v: 0
+              }
+        ]
+    }
+}
--- week8/Client/src/app/app-routing.module.ts
+++ week8/Client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, data:{title:'Home'}},
+  {path: 'about', component: AboutComponent, data:{title:'About'}},
+  {path: 'contact', component: ContactComponent, data:{title:'Contact'}},
+  {path: 'products', component: ProductsComponent, data:{title:'Products'}},
+  {path: 'services', component: ServicesComponent, data:{title:'Services'}},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
