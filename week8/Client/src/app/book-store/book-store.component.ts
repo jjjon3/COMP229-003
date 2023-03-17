db1e54fd701fa5516ecaf8b86394cec9fc72ec05
jon <janssen.nunag@gmail.com>
2023-03-17 18:11:40 -0400

    week 9 lab

--- week8/Client/src/app/book-store/book-store.component.ts
+++ week8/Client/src/app/book-store/book-store.component.ts
@@ -8,6 +8,10 @@ import { BookRepository } from '../model/book.repository';
   styleUrls: ['./book-store.component.css']
 })
 export class BookStoreComponent implements OnInit {
+  public selectedAuthor = "";
+  public booksPerPage = 4;
+  public selectedPage = 1;
+
   constructor(private repository:BookRepository) { }
   
   ngOnInit(): void {
@@ -15,10 +19,28 @@ export class BookStoreComponent implements OnInit {
   }
 
   get books():Book[] {
-    return this.repository.getBooks();
+    const pageIndex = (this.selectedPage-1)*this.booksPerPage;
+    return this.repository.getBooks(this.selectedAuthor).slice(pageIndex, pageIndex+this.booksPerPage);
   }
 
   get authors():string[] {
     return this.repository.getAuthors();
+  }
+
+  changeAuthor(newAuthor:string = ""):void {
+    this.selectedAuthor = newAuthor;
+  }
+
+  changePage(newPage:number):void {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize:number):void {
+    this.booksPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers():number[] {
+    return Array(Math.ceil(this.repository.getBooks(this.selectedAuthor).length/this.booksPerPage)).fill(0).map((x,i) => i+1);
   }
 }
--- week8/Client/src/app/model/static.datasource.ts
+++ week8/Client/src/app/model/static.datasource.ts
@@ -5,8 +5,18 @@ import { Observable, from } from 'rxjs';
 export class StaticDataSource {
     private books:Book[] = [
         new Book(1, 'Book1', 'Author 1', 'Year1', 'Short Description 1', 10),
+        new Book(1, 'Book1', 'Author 1', 'Year1', 'Short Description 1', 10),
+        new Book(1, 'Book1', 'Author 1', 'Year1', 'Short Description 1', 10),
         new Book(2, 'Book2', 'Author 2', 'Year2', 'Short Description 2', 20),
-        new Book(3, 'Book3', 'Author 3', 'Year3', 'Short Description', 30)
+        new Book(2, 'Book2', 'Author 2', 'Year2', 'Short Description 2', 20),
+        new Book(2, 'Book2', 'Author 2', 'Year2', 'Short Description 2', 20),
+        new Book(3, 'Book3', 'Author 3', 'Year3', 'Short Description', 30),
+        new Book(3, 'Book3', 'Author 3', 'Year3', 'Short Description', 30),
+        new Book(3, 'Book3', 'Author 3', 'Year3', 'Short Description', 30),
+        new Book(4, 'Book4', 'Author 4', 'Year4', 'Short Description', 40),
+        new Book(4, 'Book4', 'Author 4', 'Year4', 'Short Description', 40),
+        new Book(4, 'Book4', 'Author 4', 'Year4', 'Short Description', 40),
+        new Book(4, 'Book4', 'Author 4', 'Year4', 'Short Description', 40)
     ];
     getBooks():Observable<Book[]> {
         return from([this.books]);
